5591623ef8432cc1d54faae5105d8d4c6cefa48a
Abdulla-M-Refai <abdullah.m.refai@gmail.com>
2024-03-17 03:20:01 +0200

    created product and inventory entities

---
+++ Simple Inventory Management System/Entity/Inventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Simple_Inventory_Management_System.Entity
+{
+    public class Inventory
+    {
+        private List<Product> products = new List<Product>();
+    }
+}
---
+++ Simple Inventory Management System/Entity/Product.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Simple_Inventory_Management_System.Entity
+{
+    public class Product
+    {
+        private string name;
+
+        private double price;
+
+        private int quantity;
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Name cannot be null or empty.");
+
+                name = value;
+            }
+        }
+
+        public double Price
+        {
+            get 
+            { 
+                return price; 
+            }
+
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Price cannot be negative.");
+
+                price = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get 
+            { 
+                return quantity; 
+            }
+
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Quantity cannot be negative.");
+                
+                quantity = value;
+            }
+        }
+    }
+}
